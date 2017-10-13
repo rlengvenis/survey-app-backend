5e934a4c1ce169b3b9220730ca476ec860ec14a6
Rokas Lengvenis <rokas.lengvenis@devbridge.com>
2017-10-13 17:53:52 +0300

    change path to api

--- src/app.js
+++ src/app.js
@@ -39,8 +39,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/', users);
+app.use('/api', index);
+app.use('/api', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
