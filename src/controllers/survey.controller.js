44b816491547b9d97072631a15eea034c4b83ab2
Rokas Lengvenis <rokas.lengvenis@devbridge.com>
2017-11-03 12:55:46 +0200

    Add answers

--- src/controllers/survey.controller.js
+++ src/controllers/survey.controller.js
@@ -1,3 +1,6 @@
+import _ from 'lodash';
+import util from 'util';
+import update from 'immutability-helper';
 import Survey from '../models/survey';
 
 export const getSurvey = async (req, res) => {
@@ -31,6 +34,47 @@ export const insertOrUpdateSurvey = async (req, res) => {
     return res.send("Succesfully saved");
 
   } catch (err) {
-    return res.send(500, {error: err});
+    return res.status(500).send({error: err});
   }
+};
+
+export const updateSurveyWithAnswers = async (req, res) => {
+  try {
+    const {answers, surveyId} = req.body;
+
+    if (!answers || !surveyId) {
+      res.status(403).end();
+    }
+
+    const survey = await Survey.findOne({_id: surveyId});
+
+    const modifiedSurvey = update(survey, {
+      questions: {
+        $set: survey.questions.map(question => {
+          return _populateQuestionWithAnswers(question, answers)
+        })
+      }
+    });
+
+    await Survey.update({_id: surveyId}, modifiedSurvey);
+
+    return res.send("Succesfully saved");
+
+  } catch (err) {
+    return res.status(500).send({error: err.toString()});
+  }
+};
+
+const _populateQuestionWithAnswers = (question, answers) => {
+  const answer = _.find(answers, {questionId: question._id});
+
+  if (answer) {
+    return update(question, {
+      answers: {
+        $push: [answer]
+      }
+    });
+  }
+
+  return question;
 };
--- src/routes/survey.js
+++ src/routes/survey.js
@@ -9,6 +9,7 @@ const jwtAuthMiddleware = passport.authenticate('jwt', {session: false});
 
 router.route('/survey').get(jwtAuthMiddleware, SurveyController.getSurvey);
 router.route('/survey').put(jwtAuthMiddleware, SurveyController.insertOrUpdateSurvey);
+router.route('/answers').put(SurveyController.updateSurveyWithAnswers);
 
 
 export default router;
